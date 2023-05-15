5e2a9d4442a4f2debba5db15560d351410e5beee
askarbekov-d-auca-2022 <askarbekov_d@auca.kg>
2023-05-15 13:53:29 +0600

    Fix bugs where the cards multiplied

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const bookbtn = document.querySelector(".newbook");
 const form = document.querySelector("form");
 const submit = document.querySelector(".submit");
-const cards = document.querySelector('.book-cards')
+const cards = document.querySelector(".book-cards");
 
 let myLibrary = [];
 let i = 0;
@@ -14,27 +14,26 @@ function Book(title, author, numberOfPages, read) {
 }
 
 function displayBooks() {
-  for (let i = 0; i < myLibrary.length; i++) {
-    let bookCard = document.createElement('div');
-    bookCard.classList.add('book-style');
-    cards.appendChild(bookCard);
-    let title = document.createElement('p');
-    let author = document.createElement('p');
-    let pages = document.createElement('p');
-    let read = document.createElement('p');
-    let removebtn = document.createElement('button');
-    
-    title.textContent = `Title: ${myLibrary[i].title}`;
-    author.textContent = `Author: ${myLibrary[i].author}`;
-    pages.textContent = `Pages: ${myLibrary[i].numberOfPages}`;
-    read.textContent = `I read it: ${myLibrary[i].read}`;
-    bookCard.append(title, author, pages, read);
-  }
+  let bookCard = document.createElement("div");
+  bookCard.classList.add("book-style");
+  cards.appendChild(bookCard);
+  let title = document.createElement("p");
+  let author = document.createElement("p");
+  let pages = document.createElement("p");
+  let read = document.createElement("p");
+  let removebtn = document.createElement("button");
+  removebtn.classList.add("remove");
+  removebtn.textContent = "Remove";
+  title.textContent = `Title: ${myLibrary[i].title}`;
+  author.textContent = `Author: ${myLibrary[i].author}`;
+  pages.textContent = `Pages: ${myLibrary[i].numberOfPages}`;
+  read.textContent = `I read it: ${myLibrary[i].read}`;
+  bookCard.append(title, author, pages, read, removebtn);
 }
 
 bookbtn.addEventListener("click", () => {
   form.classList.add("visible");
-  cards.classList.add('invisible');
+  cards.classList.add("invisible");
 });
 
 submit.addEventListener("click", (event) => {
@@ -44,10 +43,9 @@ submit.addEventListener("click", (event) => {
   let pages = document.getElementById("pages");
   let read = $("#read").prop("checked");
   myLibrary[i] = new Book(title.value, author.value, pages.value, read);
-  i++;
   form.classList.remove("visible");
-  cards.classList.remove('invisible');
+  cards.classList.remove("invisible");
   console.log(myLibrary);
   displayBooks();
+  i++;
 });
-
